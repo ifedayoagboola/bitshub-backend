a8ea7a03dd765bee14a9124863028c46b2f9541c
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-08-08 20:43:10 +0100

    updated proxy to link frontend with backend

---
+++ data/categoryData.js
@@ -0,0 +1,15 @@
+export const categoryData = {
+  category: [
+    { id: "1", categoryName: "Macbook", image: "../assets/products/macbook1.jpeg" },
+    { id: "2", categoryName: "Desktop", image: "../assets/products/1646_Lenovo-Yoga-3-Pro-Repair_1.jpg" },
+    { id: "3", categoryName: "Laptop", image: "../assets/products/1525204196_635_latitude-3450.png" },
+    {
+      id: "4",
+      categoryName: "Monitor",
+      image:
+        "../assets/products/57343505b5f6d71b4fb89a419c98e47101341a14700c93a0b67f1aab668a86e6__95181.1575523312.jpg",
+    },
+    { id: "5", categoryName: "Gaming", image: "../assets/products/81nde-rFKzL._AC_SL1500_.jpg" },
+    { id: "6", categoryName: "Accessories", image: "../assets/products/A60G_131554151661089309JJQPvR7NVX.jpg" },
+  ],
+};
