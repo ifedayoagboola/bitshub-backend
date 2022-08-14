9396b6314e4a3c29cc0389bb6ea542d9247a4f12
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-08-14 10:49:22 +0100

    api for product-details

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ import express from "express";
 import ProductData from "./data/productData.js";
 
 const app = express();
+app.get("/api/products/:id", (req, res) => {
+  const product = ProductData?.products?.find((item) => item.id === Number(req.params.id));
+  if (product) {
+    res.send(product);
+  } else res.status(404).send({ message: "Product not found!" });
+});
 app.get("/api/products", (req, res) => {
-  res.send(ProductData.products);
+  res.send(ProductData?.products);
 });
 app.get("/", (req, res) => {
   res.send("server is ready");
