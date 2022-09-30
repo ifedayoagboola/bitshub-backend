ffeb9ab3a66965ae247504f1e6e56156b1aa0eb0
Ifedayo <ifedayoagboola19@gmail.com>
2022-09-30 05:06:59 +0100

    Update server.js

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: {"https://bitshub.vercel.app/"},
+    origin: "https://bitshub.vercel.app/",
     credentials: true,
   })
 );
