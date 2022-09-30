ef879895c98f3e94df1b2b129d30358cf4fec57c
Ifedayo <ifedayoagboola19@gmail.com>
2022-09-30 04:30:57 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,22 +12,6 @@ const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 4000;
 const app = express();
 
-
-app.use((req, res, next) => {
-    res.header("Access-Control-Allow-Origin", "*")
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested, Content-Type, Accept Authorization"
-    )
-    if (req.method === "OPTIONS") {
-      res.header(
-        "Access-Control-Allow-Methods",
-        "POST, PUT, PATCH, GET, DELETE"
-      )
-      return res.status(200).json({})
-    }
-    next()
-  })
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
