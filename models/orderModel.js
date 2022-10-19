3983e77c3aa00b4ca61f210815b9951e54e5a762
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-10-19 10:05:54 +0100

    modified order API

--- models/orderModel.js
+++ models/orderModel.js
@@ -6,7 +6,7 @@ const orderSchema = new mongoose.Schema(
       {
         name: { type: String, required: true },
         qty: { type: Number, required: true },
-        image: { type: String, required: true },
+        image: { type: String, required: false },
         price: { type: Number, required: true },
         product: {
           type: mongoose.Schema.Types.ObjectId,
@@ -15,18 +15,21 @@ const orderSchema = new mongoose.Schema(
         },
       },
     ],
-    shippingAddress: {
+    shippingDetails: {
       fullName: { type: String, required: true },
+      phone: { type: String, required: true },
       address: { type: String, required: true },
       city: { type: String, required: true },
-      postalCode: { type: String, required: true },
-      country: { type: String, required: true },
+      postalCode: { type: String, required: false },
+      state: { type: String, required: false },
+      country: { type: String, required: false },
+      shippingPrice: { type: String, required: true },
+      taxPrice: { type: Number, required: true },
+      totalPrice: { type: Number, required: true },
     },
     paymentMethod: { type: String, required: true },
-    itemsPrice: { type: Number, required: true },
-    shippingPrice: { type: Number, required: true },
-    taxPrice: { type: Number, required: true },
-    totalPrice: { type: Number, required: true },
+    itemsPrice: { type: Number, required: false },
+
     user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
     isPaid: { type: Boolean, default: false },
     PaidAt: { type: Date },
