c1d559a2196b202d048b14e85a495ec8831b3bcc
Ifedayo Agboola <muller@Ifedayos-MacBook-Pro.local>
2022-12-13 23:51:46 +0100

    cors error issue resolved

--- server.js
+++ server.js
@@ -7,12 +7,19 @@ import productRouter from "./routers/productRouter.js";
 import orderRouter from "./routers/orderRouter.js";
 
 dotenv.config();
+// const cors = require("cors");
 const uri = process.env.MONGODB_URI;
 const port = process.env.PORT || 4000;
 const app = express();
 
+const corsOptions = {
+  origin: "http://localhost:3000",
+  credentials: true, //access-control-allow-credentials:true
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 
 mongoose
